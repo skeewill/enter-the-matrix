29415dc77a1d033a7e8283b41a077adff908c363
Willa <willar713@gmail.com>
2019-02-14 09:40:33 -0600

    finished product with my code and Chyld's code

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from matrix import Matrix
 
 from vector import Vector
+from functools import reduce
+from collections import defaultdict
 
 
 # 1 cry
@@ -34,8 +36,8 @@ m1 = Matrix(*v_main)
 
 
 m2 = Matrix.random(3, 4)
-print(m2)
-print(m2.row, m2.col)
+# print(m2)
+# print(m2.row, m2.col)
 
 m3 = m1.scale_it(3)
 # print(m3)
@@ -46,4 +48,40 @@ m4 = m1.add_them(m1)
 # print(m4)
 # print(m4.row, m4.col)
 
-m5 = m1.mult_them(m1)
+# m5 = m1.mult_them(m1)
+
+
+with open('matrix.txt') as f:
+    matrices = []
+    vectors = []
+    for line in f:
+        line = line.strip()
+        if not line:
+            matrix = Matrix(*vectors)
+            matrices.append(matrix)
+            vectors = []
+        else:
+            vector = Vector(*map(float, line.split(' ')))
+            vectors.append(vector)
+    matrix = Matrix(*vectors)
+    matrices.append(matrix)
+
+# print all the matrices
+for i, m in enumerate(matrices):
+    print(i, 'rows: ', m.row, '\n', 'columns: ', m.col, '\n', m, '\n', '-' * 25)
+
+# filter matrices
+d = defaultdict(list)
+[d[m.shape].append(m) for m in matrices]
+print('filtered:', d)
+
+# create 5 3x3 matrices and add together
+zero = Vector(0, 0, 0)
+final = reduce(lambda acc, mat: acc.add_them(mat),
+               [Matrix.random(3, 3) for i in range(5)],
+               Matrix(zero, zero, zero))
+print('final: ', '\n', final)
+
+# find the dot product of two matrices
+dotted = d[(2, 4)][0].dot(d[(4, 2)][0])
+print('dotted: ', '\n', dotted)
--- matrix.py
+++ matrix.py
@@ -13,9 +13,10 @@ class Matrix:
         self.row = len(v)
         self.col = v[0].v_length()
         self.matrix = list(v)
+        self.shape = (len(v), v[0].v_length())
 
-    def __str__(self):
-        return "Matrix {}. \n".format(self.matrix)
+    # def __str__(self):
+    #     return "Matrix {}. \n".format(self.matrix)
 
     def __repr__(self):  # this is what is called in iPython
         return self.__str__()
@@ -24,18 +25,20 @@ class Matrix:
         return Matrix(*[v.scale(scalar) for v in self.matrix])
 
     def add_them(self, other):
+        if (self.row != other.row) or (self.col != other.col):
+            return "The matrices could not be added because they do not have the same dimensions."
         return Matrix(*[row + orow for row, orow in
                         zip(self.matrix, other.matrix)])
 
-    def mult_them(self, other):
-        m = self.row
-        n = self.col
-        for i in range(m):
-            entry = 0
-            entry = self.matrix[i]
-            print(entry)
+    # def mult_them(self, other):
+    #     m = self.row
+    #     n = self.col
+    #     for i in range(m):
+    #         entry = 0
+    #         entry = self.matrix[i]
+    #         print(entry)
 
-    @staticmethod
+    # @staticmethod
     def random(m, n):  # rewrite using for and range
         vecs = []
         for i in range(m):
@@ -44,3 +47,28 @@ class Matrix:
                 v.append(r.random())
             vecs.append(Vector(*v))
         return Matrix(*vecs)
+
+    # Chyld's
+    # @staticmethod
+    # def random(rows, cols):
+    #     randrows = [[r.random() for c in range(cols)] for r in range(rows)]
+    #     return Matrix(*[Vector(*row) for row in randrows])
+
+    def __str__(self):
+        n1 = '\n'
+        return f"[{n1.join([str(v) for v in self.matrix])}]"
+
+    def dot(self, other):
+        if (self.row != other.col) or (self.col != other.row):
+            return
+        column_vectors = [Vector(*col) for col in zip(*[m.v for m in other.matrix])]
+        transposed = Matrix(*column_vectors)
+        vectors = []
+        for row in self.matrix:
+            nums = []
+            for col in transposed.matrix:
+                scalar = row.dot(col)
+                nums.append(scalar)
+            vector = Vector(*nums)
+            vectors.append(vector)
+        return Matrix(*vectors)
